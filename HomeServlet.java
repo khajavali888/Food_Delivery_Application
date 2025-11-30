4f04306de7ccf77436beab6aaf7c5622b14e4f73
Khaja <117473981+khajavali888@users.noreply.github.com>
2025-11-30 11:44:21 +0530

    Implement HomeServlet to display restaurants

---
+++ HomeServlet.java
@@ -0,0 +1,33 @@
+package com.tap;
+import java.io.IOException;
+import java.util.List;
+import com.tap.daoimpl.RestaurantDAOImpl;
+import com.tap.model.Restaurant;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class HomeServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RestaurantDAOImpl dao = new RestaurantDAOImpl();
+
+		List<Restaurant> allRestaurants = dao.getAllRestaurants();
+
+		for (Restaurant restaurant : allRestaurants) {
+			System.out.println(restaurant);
+		}
+
+		req.setAttribute("allRestaurants", allRestaurants);
+
+		RequestDispatcher rd = req.getRequestDispatcher("home.jsp");
+		rd.forward(req, resp);
+	}
+
+}
